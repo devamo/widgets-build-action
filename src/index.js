d58fbf6139281a4be37011b1a41f1243cbdfd79b
Yaroslav Shelomentsev <ya@devio.pro>
2021-04-07 22:43:29 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
+const fs = require('fs')
+const path = require('path')
 const core = require('@actions/core')
 // const github = require('@actions/github')
 
 try {
   const githubToken = core.getInput('githubToken')
   console.log(`Github Token: ${githubToken}!`)
+
+  console.log(fs.lstatSync(path.resolve(__dirname)))
   // const time = new Date().toTimeString()
   // core.setOutput('time', time)
   // const payload = JSON.stringify(github.context.payload, undefined, 2)
