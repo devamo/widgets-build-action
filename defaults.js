be901fd028ed923158b8f4edf46afb6116eb5ba3
Yaroslav Shelomentsev <ya@devio.pro>
2021-04-08 14:43:01 +0300

    fixes

--- defaults.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  productToken:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9kdWN0SWQiOiIyMiIsImlhdCI6MTUxNjIzOTAyMn0.WRvUiJ0KhUbXseYPTf2dGDckTHF9c8xw8mFnaEWtCgM',
-  productVersion: 'v1.2',
-  bundleFolder: '/Users/mac/Desktop/Projects/amodev/widgets-build-action/demo/build',
-  sourcesFolder: '/Users/mac/Desktop/Projects/amodev/widgets-build-action/demo'
-}
